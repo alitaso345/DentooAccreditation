d68b99f891ad32477767a4fc34a446aaecbc824c
alice345 <alice.maru345@gmail.com>
2014-04-16 19:57:12 +0900

    各シートの情報をsheetsに格納

--- DentooAccreditation.rb
+++ DentooAccreditation.rb
@@ -1,9 +1,18 @@
 require 'spreadsheet'
 
 class DentooAccreditation
+  SHEETNUM = 16
+
   def initialize
     Spreadsheet.client_encoding = "UTF-8"
     @book = Spreadsheet.open('./H24nintei.xls')
+  end
+
+  def edit
+    sheets = Array.new(SHEETNUM)
+    SHEETNUM.times do |sheet_number|
+      sheets[sheet_number] = @book.worksheet sheet_number
+    end
   end
 
   def save
@@ -13,4 +22,4 @@ class DentooAccreditation
 
 end
 
-DentooAccreditation.new.save
+DentooAccreditation.new.edit
