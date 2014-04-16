10aa9734d372b2b9a0a722b48901e7f756a9f367
alice345 <alice.maru345@gmail.com>
2014-04-16 21:23:58 +0900

    コマンドライン引数から各値を入力できるように変更

--- DentooAccreditation.rb
+++ DentooAccreditation.rb
@@ -6,6 +6,14 @@ class DentooAccreditation
   def initialize
     Spreadsheet.client_encoding = "UTF-8"
     @book = Spreadsheet.open('./H24nintei.xls')
+    @course = ARGV[0]
+    @name = ARGV[1]
+    @examinees_number = ARGV[2]
+    @student_id = ARGV[3]
+    @graduated_school_name = ARGV[4]
+    @graduated_school_course = ARGV[5]
+    @entrance_year = ARGV[6]
+    @graduated_year = ARGV[7]
   end
 
   def edit
@@ -21,14 +29,14 @@ class DentooAccreditation
     end
 
     sheets.each do |sheet|
-      sheet[5,8] = "情報・通信工"
-      sheet[5,15] = "ありたそ"
-      sheet[5,22] = "0000"
-      sheet[6,22] = "123456"
-      sheet[10,2] = "長岡工業" + "高等専門学校"
-      sheet[9,13] = "電子制御工学科"
-      sheet[8,18] = "      " + "21" + "年       " + "3月 入学"
-      sheet[10,18] = "      " + "26" + "年       " + "4月 卒業・卒業見込・退学"
+      sheet[5,8] = @course
+      sheet[5,15] = @name
+      sheet[5,22] = @examinees_number
+      sheet[6,22] = @student_id
+      sheet[10,2] = @graduated_school_name + "高等専門学校"
+      sheet[9,13] = @graduated_school_course
+      sheet[8,18] = "      " + @entrance_year + "年       " + "3月 入学"
+      sheet[10,18] = "      " + @graduated_year + "年       " + "4月 卒業・卒業見込・退学"
     end
 
     save()
