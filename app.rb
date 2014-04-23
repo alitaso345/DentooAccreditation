a34116bab32f1ae71b5629398f029aef4427c5a8
alitaso345 <alice.maru345@gmail.com>
2014-04-23 10:07:45 +0900

    データをDentooAccreditationで扱える形に整形するためのAdapter.rbを作成

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
-require 'thor'
 require_relative 'lib/DentooAccreditation'
+require_relative 'lib/Adapter'
 
+#arguments = Adapterを使ってユーザから入力されたものを，Dentooaccreditationでxlsで扱える形(今回はhash)に変換してやる
+#とりあえず今は適当な変数を使ってる
 arguments = {:course => "情報通信工学科",:name => "alitaso",:entrance_year => 21,:graduated_year => 26,:graduated_school_name => "サレジオ"}
 
 DentooAccreditation.new(arguments).edit
