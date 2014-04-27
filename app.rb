9f73cfed44cf93f83f95a71f61390a1d2c51c6a1
alitaso345 <alice.maru345@gmail.com>
2014-04-28 02:36:41 +0900

    オプションパーサーを用いたアダプターを作成

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require_relative 'lib/Adapter'
 
 #arguments = Adapterを使ってユーザから入力されたものを，Dentooaccreditationでxlsで扱える形(今回はhash)に変換してやる
 #とりあえず今は適当な変数を使ってる
-arguments = {:course => "情報通信工学科",:name => "alitaso",:entrance_year => 21,:graduated_year => 26,:graduated_school_name => "サレジオ"}
+
+arguments = Adapter::CommandLineAdapter.start(ARGV)
 
 DentooAccreditation.new(arguments).edit
--- lib/Adapter.rb
+++ lib/Adapter.rb
@@ -1,7 +1,19 @@
 require 'thor'
 
 class Adapter
-  class CommandLineAdapter
+  class CommandLineAdapter < Thor
+    desc "CommandLineAdapter","test"
+    option :course
+    option :name
+    option :examinees_number
+    option :student_id
+    option :graduated_school_name
+    option :graduated_school_course
+    option :entrance_year
+    option :graduated_year
 
+    def make_options
+      return options
+    end
   end
 end
